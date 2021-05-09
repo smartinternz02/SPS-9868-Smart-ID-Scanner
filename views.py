260d95e31b1bfcced4d12f22648e5e5ee4f0aac2
Igousiya <60340864+Igousiya@users.noreply.github.com>
2021-05-09 18:41:13 +0530

    Create views.py

---
+++ views.py
@@ -0,0 +1,17 @@
+import json, shutil, requests
+
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+
+def get_attendence(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+        
+    return text
